cbbf17458bb3c80ab11824aad3fa52cfc91c5d9f
Quadrosh <quadrosh@gmail.com>
2017-03-04 16:26:39 +0300

    sql user

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=tzy',
-    'username' => 'tb303',
-    'password' => '303',
+    'username' => 'transzakazsql',
+    'password' => 'iQaazQW9hbyuoUZI',
     'charset' => 'utf8',
 ];
