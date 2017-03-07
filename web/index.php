c817beffc4949f3fc15c71cc0bd17c4d8bd35f57
Quadrosh <quadrosh@gmail.com>
2017-03-07 13:36:09 +0300

    index back

---
+++ web/index.php
@@ -0,0 +1,18 @@
+<?php
+
+// comment to production
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
+
+define('YII_ENABLE_ERROR_HANDLER', true);
+define('YII_ENABLE_EXCEPTION_HANDLER', true);
+
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require_once __DIR__ . '/../functions.php' ;
+
+$config = require(__DIR__ . '/../config/web.php');
+
+(new yii\web\Application($config))->run();
+
