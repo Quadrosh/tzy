cb039457cf0276963b005451c39f872c80519350
Quadrosh <quadrosh@gmail.com>
2017-11-18 12:49:55 +0300

    local params 0.4

--- config/local_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=tzy',
-    'username' => 'transzakazsql',
-    'password' => 'iQaazQW9hbyuoUZI',
-    'charset' => 'utf8',
-];
--- config/local_params.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'adminEmail' => 'quadrosh@gmail.com',
-    'cookieValidationKey' => 'Oqt7HoFcW8hC7Pe7KDhyOQiR1U-JQb88',
-];
