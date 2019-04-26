297a2b433cda650d3bc9efd5a2f66f1a0e192545
Quadrosh <quadrosh@gmail.com>
2019-04-26 13:26:53 +0300

    footer vk fb

--- tszakaz_ru/views/layouts/main.php
+++ tszakaz_ru/views/layouts/main.php
@@ -200,73 +200,11 @@ $feedback = new \common\models\Feedback();
 
 </div>
 
-<footer class="footer_">
-    <div class="container footer">
-        <div class="row">
-            <div class="col-xs-6 col-sm-4 b-footer__info">
-                <div class="b-footer__info__content">
-                    <p>&copy; Транспортная компания ООО &ldquo;Трансзаказ&rdquo;, <?= date('Y') ?><br/>
-                        117535, Москва, 3-й дорожный проезд, д.3а<br/>
-                        Телефон: +7 (495) 150-05-83</p>
-                </div>
-            </div>
-            <div class="col-xs-6 col-sm-8 b-footer__menu">
-                <div class="col-sm-3 text-center ">
-                    <a href="/about.html">О компании</a>
-                </div>
-                <div class="col-sm-3 text-center ">
-                    <a href="/services.html">Услуги</a>
-                </div>
-                <div class="col-sm-3 text-center ">
-                    <a href="/contacts.html">Контакты</a>
-                </div>
-                <div class="col-sm-3 text-center hidden-xs">
-                    <a href="/sitemap.html">Карта сайта</a>
-                </div>
-
-
-            </div>
-
-        </div>
-
-    </div><!-- /.footer -->
-</footer>
+<?= $this->render('/layouts/footer', []) ?>
 
 <?php $this->endBody() ?>
-<!-- Yandex.Metrika counter -->
-<script type="text/javascript">
-    (function (d, w, c) {
-        (w[c] = w[c] || []).push(function() {
-            try {
-                w.yaCounter42636264 = new Ya.Metrika({
-                    id:42636264,
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true
-                });
-            } catch(e) { }
-        });
 
-        var n = d.getElementsByTagName("script")[0],
-            s = d.createElement("script"),
-            f = function () { n.parentNode.insertBefore(s, n); };
-        s.type = "text/javascript";
-        s.async = true;
-        s.src = "https://mc.yandex.ru/metrika/watch.js";
-
-        if (w.opera == "[object Opera]") {
-            d.addEventListener("DOMContentLoaded", f, false);
-        } else { f(); }
-    })(document, window, "yandex_metrika_callbacks");
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/42636264" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
-
-<!-- Yandex.Metrika NEW counter -->
-<script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter30134129 = new Ya.Metrika({ id:30134129, clickmap:true, trackLinks:true, accurateTrackBounce:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/30134129" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
-
-
+<?php include_once("analytics_yandex.php") ?>
 
 </body>
 </html>
