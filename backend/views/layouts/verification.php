f577f15c73c309400823f539dc72fb73b99c4a2e
Quadrosh <quadrosh@gmail.com>
2019-08-14 12:53:52 +0300

    fix verification layout back

--- backend/views/layouts/verification.php
+++ backend/views/layouts/verification.php
@@ -38,8 +38,6 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
 
-            ['label' => 'Заявки - качество', 'url' => ['/preorders/lead-quality']],
-
             Yii::$app->user->isGuest ? (
             ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
